4e17a71bf22efed39d69f9ea1c2dd4a0e02a1ec0
Shahzad-Ansari <34826823+Shahzad-Ansari@users.noreply.github.com>
2019-05-24 18:23:04 -0500

    creating node class for hexapawn board states

--- BoardStates/HexaPawn_Node.py
+++ BoardStates/HexaPawn_Node.py
@@ -3,6 +3,23 @@ class HexaPawnNode:
         self.board = board
         self.parent = parent_node
         self.move = move
+        setPuzzle(board)
 
-    def
+    children_list = ()
+    x = 0
+    col = 3
+    row = 3
+    depth = None
+    heuristic = None
+    Fcost = None
+
+    def setPuzzle(self , current_board ):
+        for board in current_board:
+            self.board_state = current_board
+
+    def goalTest(self):
+        for
+
+
+
 
